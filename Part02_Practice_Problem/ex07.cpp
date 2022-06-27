3374595c5449f4742390dcae170176f23ee6cc76
이두산 <sxfc675@gmail.com>
2022-06-27 21:33:22 +0900

    [22.05.22] 3장 7번 연습문제 풀이

---
+++ Part02_Practice_Problem/ex07.cpp
@@ -0,0 +1,16 @@
+/* [22.06.27]
+문제 5번을 참고하여 생성자를 이용하여 짝수 홀수를 선택할 수 있도록 
+SelectableRandom 클래스를 작성하고 
+짝수 10개, 홀수 10개를 랜덤하게 발생시키는 프로그램을 작성하라.
+*/
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+
+
+int main() {
+    
+}
