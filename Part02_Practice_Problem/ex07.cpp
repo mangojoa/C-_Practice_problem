7837a86c706ac74330039f638661124209341519
이두산 <sxfc675@gmail.com>
2022-06-28 17:05:18 +0900

    [22.05.22] 3장 7번 연습문제 풀이

--- Part02_Practice_Problem/ex07.cpp
+++ Part02_Practice_Problem/ex07.cpp
@@ -9,8 +9,65 @@ SelectableRandom 클래스를 작성하고
 #include <cstdlib>
 using namespace std;
 
+class SelectableRandom {
+private: 
+    int a;
+public: 
+    SelectableRandom(int b);
+    int next();
+    int nextInRange(int x, int y);
+};
 
+SelectableRandom::SelectableRandom(int b) {
+    b = a;
+    srand((unsigned)time(0));
+}
+
+int SelectableRandom::next() {
+    int r_num = rand();
+    if(a%2 == 0){ // 짝수 return 
+        if(r_num%2 == 0) 
+            return r_num;
+        else 
+            return --r_num;
+    }
+    else {
+        if(r_num%2 == 0) // 홀수 return 
+            return ++r_num;
+        else
+            return r_num;
+    }
+}
+
+int SelectableRandom::nextInRange(int x, int y) {
+    int r_num = rand()%(y-x+1) + x;
+    if(a%2 == 0) { // 짝수 return
+        if(r_num%2 == 0) 
+            return r_num;
+        else 
+            return ++r_num;
+    }
+    else { // 홀수 return 
+        if(r_num%2 == 0) 
+            return ++r_num;
+        else 
+            return r_num;
+    }
+}
 
 int main() {
+    SelectableRandom r1(0); // 짝수 랜덤 정수 발생
+    cout << "-- 0에서 " << RAND_MAX << "까지의 짝수 랜덤 정수 10 개--" << endl;
+    for(int i=0; i<10; i++) {
+        int n = r1.next(); 
+        cout << n << ' ';
+    } 
     
+    SelectableRandom r2(1); // 홀수 랜덤 정수 발생
+    cout << endl << endl << "-- 2에서 " << "9 까지의 랜덤 홀수  정수 10 개 --" << endl;
+    for(int i=0; i<10; i++) {
+        int n = r2.nextInRange(2,9);
+        cout << n << ' ';
+    }
+    cout << endl;
 }
